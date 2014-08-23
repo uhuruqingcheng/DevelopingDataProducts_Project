7a8e4d2e2c16e9a2229f7885804eeed16ab02f5c
uhuruqingcheng <xiang_lqc@126.com>
2014-08-24 04:58:22 +0800

    Change the README and add shinyApp

--- README.md
+++ README.md
@@ -2,3 +2,24 @@ DevelopingDataProducts_Project
 ==============================
 
 Project for the class Developing Data Products in Coursera, Shiny App.
+
+Couresra-[Developing Data Product][devdataprod-004][Course Project]
+
+This peer assessed assignment has two parts. First, we need to create a Shiny application and deploy it on Rstudio's servers. Second, use Slidify or Rstudio Presenter to prepare a reproducible pitch presentation about your application.
+
+
+Therefore, this repo has two part:
+
+## 1. Shiny Application
+You can access my app here: http://uhuruqingcheng.shinyapps.io/shiny
+
+The R code of this app is in the folder **shinyApp_LEB**. More information of this app can be obtained in the document][1]. 
+
+
+## 2. Reproducible Pitch Presentation
+
+ After have made my shiny app, I need to make my pitch. I get 5 slides (inclusive of the title slide)  to pitch my app. You can get the pitch by web page using Slidify. This is the [link][2]
+
+
+ [1]:http://uhuruqingcheng.shinyapps.io/shiny/_w_4d45f2ac1e8864695fabb7d13c87e95933f9465400809eb7/README.html
+ [2]:http://uhuruqingcheng.github.io/DevelopingDataProducts_Project/
---
+++ shinyApp_LEB/server.R
@@ -0,0 +1,55 @@
+LEB<-function(country, gender){
+    if(gender=="Male")return(mean(LEBmaleCat$Value[LEBmaleCat$country==country]))
+    else return(mean(LEBfemaleCat$Value[LEBmaleCat$country==country]))
+}
+
+LEBCategories<-function(ccountry,gender){
+    if(gender=="Male"){temp <- LEBmaleCat}
+    else{temp <- LEBfemaleCat}
+    temp<-subset(temp,temp$country==ccountry)
+    temp<-temp[order(temp$Year,decreasing=TRUE),c("Year","gender","Value")]
+    return(temp)
+}
+LEBTrend<-function(ccountry){
+    temp1<-subset(LEBmaleCat,LEBmaleCat$country==ccountry,select=c("Year","Value","gender"))
+    temp2<-subset(LEBfemaleCat,LEBfemaleCat$country==ccountry,select=c("Year","Value","gender"))
+    temp<- rbind(temp1,temp2)
+    temp<-temp[order(c(temp$Year,temp$gender)),]
+    names(temp)<-c("Year","Life Expectancy at Birth Value","gender")
+    return(temp)
+}
+library(shiny)
+library(reshape2)
+LEBmaleCat <- read.csv("data/LifeExpectancyAtBirth_male.csv")
+LEBfemaleCat <- read.csv("data/LifeExpectancyAtBirth_female.csv")
+LEBmaleCat$Year<-as.numeric(format(as.Date(as.character(LEBmaleCat$Date),format="%d/%m/%Y"),"%Y"))
+LEBfemaleCat$Year<-as.numeric(format(as.Date(as.character(LEBfemaleCat$Date),format="%d/%m/%Y"),"%Y"))
+LEBmaleCat$gender<- factor("male")
+LEBfemaleCat$gender<- factor("female")
+
+shinyServer(
+    function(input, output) { 
+        output$oiLEB <- renderPrint({
+            input$goButton
+            isolate(LEB(input$country,input$gender))
+        })
+        output$oicountry<-renderText({
+            input$goButton
+            isolate(input$country)
+        })
+        output$oigender<-renderText({
+            input$goButton
+            isolate(input$gender)
+        })  
+        output$oiLEBcattable = renderDataTable({
+            input$goButton
+            isolate(LEBCategories(input$country,input$gender))
+        })
+        output$Plot1 <- renderChart({
+            input$goButton
+            isolate(datat<-LEBTrend(input$country))
+            hp1 <- hPlot(x = "Year", y = "Life Expectancy at Birth Value",group="gender", data = datat, type = "line")
+            hp1$addParams(dom ='Plot1')
+            return(hp1)
+        })
+    })  
